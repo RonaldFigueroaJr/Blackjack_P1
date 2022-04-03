e735f81be4c54d2f43d217a21ab97a42dc203548
ronaldjr <figueroajrronald@gmail.com>
2022-04-02 21:45:07 -1000

    Added more const and element variable holders.

---
+++ main.js
@@ -0,0 +1,22 @@
+// Creating types and suits of cards
+const suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+// Variables for gameplay
+const playerBalance = 5000;
+const endGame = false;
+const playerCards = []; // array to hold cards help by player
+const dealerCards = []; // to hold cards held by dealer
+const cardCount = 0; // to know which card we are at
+const playerBal = 5000;
+
+
+// Elements
+const pCards = document.getElementById('pCards');
+const dCards = document.getElementById('dCards');
+
+
+
+
+
+
